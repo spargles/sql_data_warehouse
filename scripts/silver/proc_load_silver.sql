4c7da5e974571588793114fe30d19b680f63d131
spargles <43338628+spargles@users.noreply.github.com>
2026-08-16 18:10:36 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -87,7 +87,7 @@ BEGIN
 
     SELECT 
     prd_id, 
-    SUBSTRING(prd_key, 1,5) AS cat_id, 
+    REPLACE(SUBSTRING(prd_key, 1,5), '-', '_') AS cat_id,  AS cat_id, 
     SUBSTRING(prd_key, 7) AS prd_key,
     prd_nm, 
     ISNULL(prd_cost, 0) AS prd_cost, 
